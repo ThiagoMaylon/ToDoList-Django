92abe8f59a5b56fb87df92b664bed56e51690c46
Thiago Maylon <tmaylon88@gmail.com>
2024-01-14 15:42:41 -0300

    Adiconado botão de deletar item

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import TarefaList
+from .views import TarefaList, TarefaDeleteList
 
 urlpatterns = [
-    path('', TarefaList.as_view(), name='index')
+    path('', TarefaList.as_view(), name='index'),
+    path('tarefa/delete/<int:pk>', TarefaDeleteList.as_view(), name='delete')
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,24 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DeleteView
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
+
 from .models import Tarefa
 
 class TarefaList(ListView):
     model = Tarefa
     template_name = 'index.html'
     context_object_name = 'tarefas'
-    
+
+class TarefaDeleteList(DeleteView):
+    model = Tarefa
+    success_url = reverse_lazy('index')
+    template_name = 'index.html'
+    context_object_name = 'tarefa'
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.delete()
+        return redirect(self.get_success_url())
