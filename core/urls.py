2b16883954308cd015a46350f3f9133fe0d70bbf
Thiago Maylon <tmaylon88@gmail.com>
2024-01-15 10:11:37 -0300

    Adicionando botao de criar novo item

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import TarefaList, TarefaDeleteList
+from .views import TarefaList, TarefaDeleteList, TarefaCreateList
 
 urlpatterns = [
     path('', TarefaList.as_view(), name='index'),
-    path('tarefa/delete/<int:pk>', TarefaDeleteList.as_view(), name='delete')
+    path('tarefa/delete/<int:pk>', TarefaDeleteList.as_view(), name='delete'),
+    path('tarefa/create/', TarefaCreateList.as_view(), name='create')
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import ListView, DeleteView
+from django.forms.models import BaseModelForm
+from django.http import HttpResponse
+from django.views.generic import ListView, DeleteView, CreateView
 from django.urls import reverse_lazy
 from django.shortcuts import redirect
 
@@ -8,6 +10,16 @@ class TarefaList(ListView):
     model = Tarefa
     template_name = 'index.html'
     context_object_name = 'tarefas'
+
+class TarefaCreateList(CreateView):
+    model = Tarefa
+    success_url = reverse_lazy('index')
+    template_name = 'index.html'
+    fields = ['tarefa']
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        return super().form_valid(form)
+
 
 class TarefaDeleteList(DeleteView):
     model = Tarefa
